c80e8be88d64537cf8ca3e4e99b3eb7ee7843732
siam-khan-alt <nssiam99@gmail.com>
2026-05-10 11:30:34 +0600

    [Added]: add next auth secret

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -6,6 +6,7 @@ import bcrypt from "bcryptjs";
 import { User } from "@/models/User";
 
 export const authOptions: NextAuthOptions = {
+  secret: process.env.NEXTAUTH_SECRET,
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
