afde96990799be405bd02f9cd4672128ad893b9e
siam-khan-alt <nssiam99@gmail.com>
2026-05-25 15:56:30 +0600

    [Fixed]: fix error in the api course id

--- src/app/api/courses/[id]/route.ts
+++ src/app/api/courses/[id]/route.ts
@@ -5,18 +5,21 @@ import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import { connectDB } from "@/shared/lib/db";
 import { Course } from "@/models/Course";
 
-export async function PUT(req: NextRequest, { params }: { params: { id: string } }) {
+interface RouteContext {
+  params: Promise<{ id: string }>;
+}
+export async function PUT(req: NextRequest,context: RouteContext) {
   try {
     const session = await getServerSession(authOptions);
     if (!session || session.user.role !== "admin") {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-
+    const { id } = await context.params;
     await connectDB();
     const body = await req.json();
     
     const updatedCourse = await Course.findByIdAndUpdate(
-      params.id,
+      id,
       {
         title: body.title,
         description: body.description,
@@ -38,15 +41,15 @@ export async function PUT(req: NextRequest, { params }: { params: { id: string }
   }
 }
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+export async function DELETE(req: NextRequest,context: RouteContext) {
   try {
     const session = await getServerSession(authOptions);
     if (!session || session.user.role !== "admin") {
       return NextResponse.json({ error: "Unauthorized" }, { status: 401 });
     }
-
+  const { id } = await context.params;
     await connectDB();
-    const deletedCourse = await Course.findByIdAndDelete(params.id);
+    const deletedCourse = await Course.findByIdAndDelete(id);
 
     if (!deletedCourse) {
       return NextResponse.json({ error: "Course not found" }, { status: 404 });
